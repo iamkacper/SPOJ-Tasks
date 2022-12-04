745496959a99bb984a3835dcbc329f5b16067907
iamkacper <kacper.pys@icloud.com>
2022-12-04 13:51:33 +0100

    ;)

--- ONP - Transform the Expression/Program.cs
+++ ONP - Transform the Expression/Program.cs
@@ -1,2 +1,46 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace ONP
+{
+    class TransformTheExpression
+    {
+
+        static void Main()
+        {
+            int T = int.Parse(Console.ReadLine());
+            for (int i = 0; i < T; i++)
+            {
+                ConvertExpression();
+            }
+        }
+
+        public static void ConvertExpression()
+        {
+            string exp = Console.ReadLine();
+            if (exp == null)
+            {
+                throw new Exception("expression cannot be null");
+            }
+            StringBuilder result = new StringBuilder();
+            Stack<char> symbols = new Stack<char>();
+            foreach (char ch in exp)
+            {
+                if (ch == '(')
+                    continue;
+                else if (ch == ')')
+                    result.Append(symbols.Pop());
+                else if (char.IsLetterOrDigit(ch))
+                    result.Append(ch);
+                else
+                    symbols.Push(ch);
+            }
+            Console.WriteLine(result);
+
+        }
+    }
+}
